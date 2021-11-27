fc59683fca518c1a8e15f84363f7346d914fa712
iorgen <test>
2021-11-27 15:57:19 +0300

    ssl cert fix

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(178.79.133.32)
+domains=(holivator.com www.holivator.com)
 rsa_key_size=4096
 data_path="./security/certbot"
 email="" # Adding a valid address is strongly recommended
